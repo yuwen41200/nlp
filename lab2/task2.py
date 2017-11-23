0981ba533c63e57d073b8d8e7c0d21e146f4de7c
Yu-wen Pwu <ywpu@cs.nctu.edu.tw>
2017-11-24 04:18:31 +0800

    Done I guess.

--- lab2/task2.py
+++ lab2/task2.py
@@ -4,6 +4,7 @@
 import jieba
 import jieba.posseg as pseg
 import json
+import math
 
 jieba.set_dictionary('dict.txt.big')
 jieba.enable_parallel(4)
@@ -29,5 +30,58 @@ def parser(filename):
         json.dump(observations, f, ensure_ascii=False, indent='\t')
 
 
+def analyzer1():
+    counter = {}
+    with open('task2.json') as f:
+        observations = json.load(f)
+    print('N =', len(observations))
+    ls = [term[0] + term[1] for line in observations for term in line]
+    for l in ls:
+        if l not in counter:
+            counter[l] = 0
+        counter[l] += 1
+    for k, v in counter.items():
+        print(k, v)
+
+
+def probability(observations, word1, flag1, word2=None, flag2=None):
+    count = 0
+    N = len(observations)
+    if not word2:
+        word2, flag2 = word1, flag1
+    for line in observations:
+        match1, match2 = False, False
+        for term in line:
+            if term[0] == word1 and term[1] == flag1:
+                match1 = True
+            if term[0] == word2 and term[1] == flag2:
+                match2 = True
+        if match1 and match2:
+            count += 1
+    return count / N
+
+
+def pointwise_mutual_information(observations, word1, flag1, word2, flag2):
+    Px = probability(observations, word1, flag1)
+    Py = probability(observations, word2, flag2)
+    Pxy = probability(observations, word1, flag1, word2, flag2)
+    PMI = math.log2(Pxy / (Px * Py))
+    print(word1, flag1, Px, sep='\t')
+    print(word2, flag2, Py, sep='\t')
+    print(PMI)
+    print()
+    return PMI
+
+
+def analyzer2():
+    with open('task2.json') as f:
+        observations = json.load(f)
+    pointwise_mutual_information(observations, '喜欢', 'v', '妳', 'zg')
+    pointwise_mutual_information(observations, '打', 'v', '手枪', 'n')
+    pointwise_mutual_information(observations, '追', 'v', '她', 'r')
+
+
 if __name__ == '__main__':
-    parser('You Are the Apple of My Eye SC.srt')
+    # parser('You Are the Apple of My Eye SC.srt')
+    # analyzer1()
+    analyzer2()
