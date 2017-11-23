61958a3da8d826e99b2d9903493e8e2436a00950
Yu-wen Pwu <ywpu@cs.nctu.edu.tw>
2017-11-23 15:11:11 +0800

    fuck don't change the question after i already finished.

--- lab2/task3.py
+++ lab2/task3.py
@@ -13,20 +13,18 @@ def parser(filename):
     with open(filename) as f:
         for line in f:
             words = pseg.cut(line.strip())
-            words = [(word, flag[0]) for word, flag in words if flag[0] != 'x']
-            for i in range(len(words)-1):
-                key = words[i][1] + words[i+1][1]
-                value = words[i][0] + words[i+1][0]
-                if key not in pos_pattern:
-                    pos_pattern[key] = []
-                pos_pattern[key].append(value)
+            keys = [flag[0] for word, flag in words if flag[0] != 'x']
+            key_str = '(' + ', '.join(keys) + ')'
+            value_str = line.strip()
+            if key_str not in pos_pattern:
+                pos_pattern[key_str] = []
+            pos_pattern[key_str].append(value_str)
 
 
 if __name__ == '__main__':
     parser('年度最高票房紀錄列表簡中.csv')
     for _ in range(3):
         m = max(pos_pattern, key=lambda x: len(pos_pattern[x]))
-        title = '(' + m[0] + ', ' + m[1] + ')'
         ans = '\t'.join(pos_pattern[m])
-        print(title, len(pos_pattern[m]), ans, sep='\t')
+        print(m, len(pos_pattern[m]), ans, sep='\t')
         del pos_pattern[m]
